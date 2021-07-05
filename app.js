e5c2a036e95027613679552c823569e291dbe332
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-05 14:34:19 -0300

    Experiment on code being executed by client and server

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var people = [
+	{ name: 'Rudolph Reindeer'      },
+	{ name: 'Tree Trunks'           },
+	{ name: 'Mr. Potato Roosevelt'  }
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	
-	res.render('index');
+	res.render('index', { serverPeople: people });
 	
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,8 @@ angular.module('TestApp', []);
 angular.module('TestApp')
     .controller('MainController', ctrlFunc);
 
-function ctrlFunc() {
-    this.message = 'Hello there hope everything is fine with you';
 
-    this.people = [
-        { name: 'Rudolph Reindeer'      },
-        { name: 'Tree Trunks'           },
-        { name: 'Mr. Potato Roosevelt'  }
-    ]; 
+
+function ctrlFunc() {
+    this.people = clientPeople; 
 }
